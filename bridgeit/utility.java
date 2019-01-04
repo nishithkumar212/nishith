0caa5ca255da62145cb129aea70a2c1ab5813e81
nishithkumar212 <46148406+nishithkumar212@users.noreply.github.com>
2019-01-04 18:42:49 +0530

    Add files via upload

--- bridgeit/utility.java
+++ bridgeit/utility.java
@@ -42,6 +42,11 @@ public class utility
 	{
 		return System.currentTimeMillis();
 	}
+	public static boolean getboolean()
+	{
+		boolean a=sc.nextBoolean();
+		return a;
+	}
 public static int  binarysearch(int[] arr,int key)
 {
 	int start=0;
@@ -150,29 +155,26 @@ public static void bubblesortinteger(int[] arr)
 	public static void insertionsortString(String[] arr)
 	{
 		int n=arr.length;
-		for(int i=1;i<n;i++)
-		{
-			String key=arr[1];
-			int j=i-1;
-			while(j>=0)
-			{
-				if(key.compareTo(arr[j])<0);
-				{
+		System.out.println(Arrays.toString(arr));
+		System.out.println("elements");
+		for(int i=1;i<arr.length;i++) {
+			String key = arr[i];
+			int j = i-1;
+			while(j>=0) {
+				if(key.compareTo(arr[j])>0) {
+					break;
 				}
-				arr[j+1]=arr[j];
-				j=j-1;
+				arr[j+1] = arr[j];
+				j--;
 			}
-				arr[j+1]=key;
-			    }
-		for(int k=0;k<=arr.length-1;k++)
-		{
-			System.out.print(arr[k]);
+			arr[j+1] =key;
+			System.out.println(Arrays.toString(arr));
 		}
+		System.out.println(Arrays.toString(arr));
 	}
 	public static  void readstring(String path,String divide) {
 		String line = null;
 		String[] words = null;
-		
 		try {
 		InputStream is =new FileInputStream(path);
 		InputStreamReader isr = new InputStreamReader(is);
@@ -226,13 +228,151 @@ public static void bubblesortinteger(int[] arr)
 		brr[i] = Integer.parseInt(sr1[i]);
 	}
 	utility.bubblesortinteger(brr);
-
 	}
-	
-	
-	
+	public  static void datecalculate(int m,int d,int y)
+	{
+		int y0;
+		int x;
+		int m0;
+		int d0;
+		y0 = y - (14 - m) / 12;
+				x = y0 + (y0/4 )-( y0/100 )+ y0/400;
+				m0 = m + 12 * ((14 - m) / 12) -2;
+				d0 = (d + x + 31*m0/ 12)%7;
+switch(d0)
+{
+case 0:System.out.println(" the day falls on:sunday");
+break;
+case 1:System.out.println("the day falls on:monday");
+break;
+case 2:System.out.println("the day falls on:tuesday");
+break;
+case 3:System.out.println("the day falls on:wednesday");
+break;
+case 4:System.out.println("the day falls on:thursday");
+break;
+case 5:System.out.println("the day falls on:friday");
+break;
+case 6:System.out.println("the day falls on:saturday");
+break;
+}
+}
+public static void   temperatureconvert(double  temp)
+{
+	if(temp>=90.00)
+	{
+		double f=temp;
+		double c=(f - 32) * 5/9;
+	System.out.println("the converted fahrenheit to celcius is:"+c);
+	}
+	else
+	{
+		double c=temp;
+	double  f=(c * 9/5)+ 32;
+System.out.println("the converted celcius to fahrenheit is:"+f);
+	}
+}
+static int i;
+static int total;
+public  static int  vendingcalculate(int money,int[] notes)
+{
+	int calnotes;
+	int r;
+	if(money==0)
+	{
+		return -1;
+	}
+	else
+	{
+		if(money>=notes[i])
+		{
+	 calnotes=money/notes[i];	
+	r=money%notes[i];
+	money=r;
+	total=total+calnotes;
+	System.out.println(notes[i]+"notes are:"+calnotes);
+	}
+	i++;
+	vendingcalculate(money,notes);
+}
+	return total;
+}
+public static double monthlypayment(double p,double y,double r)
+{
+	double n=12*y;
+	r=r/(12*100);
+	double num=p*r;
+double  power=(1-Math.pow(1+r, -n));
+	double payment=num/power;
+	return payment;
+	}
+public static void insertionstring(String path,String divide) throws IOException
+{
+	String line;
+	String[] words = null;
+FileInputStream fis=new FileInputStream(path);
+InputStreamReader is=new InputStreamReader(fis);
+BufferedReader br=new BufferedReader(is);
+while((line=br.readLine())!=null)
+{
+	words=line.split(divide);
+}
+System.out.println(words);
+utility.insertionsortString(words);
+}
+public static void squareroot(double c)
+{
+	double t=c;
+	 double epsilon = 1e-15;
+		while(Math.abs(t -c/t)>epsilon*t)
+		{
+			t=(c/t+t)/2;
+		}
+	System.out.println(t);
+}
+public static int findNumber(int arr[],int start,int end) 
+{
+while(start<=end)
+{
+	int mid=(start+end)/2;
+	System.out.println("you number is "+arr[mid]+" if yes enter true,else false");
+	Boolean  a=utility.getboolean();
+	if(a==true)
+	{
+return mid;
+	}
+	System.out.println("your number is greater than "+arr[mid]+"if yes enter true,else false");
+	boolean b=utility.getboolean();
+	if(b==true)
+	{
+		start=mid++;
+	}
+	else
+	{
+		end=mid--;
+	}
+}
+return -1;
+}
 	}
 
+		
 	
+
+
 	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
